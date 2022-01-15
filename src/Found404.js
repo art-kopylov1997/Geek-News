c5238024e4c3724402193693605b0ab2b4562fb5
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-01-15 23:07:19 +0300

    refactoring

--- src/Found404.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Found404 = () => {
-    return (
-        <h1>404 FOUND</h1>
-    )
-}
-
-export default Found404;
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NotFound.module.scss"
+
+const NotFound = () => (
+    <h1>PAGE NOT FOUND...</h1>
+);
+
+export default NotFound;
